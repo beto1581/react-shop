5a596f0029816fac23c8b3ea4d882ced9be38177
beto1581 <nelsoncortezrueda1993@gmail.com>
2022-06-29 17:00:51 -0600

    adding page, modules, containers

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className="NotFound">
-            not found
+        <div>
+            Not Found
         </div>
     );
 };
